b63061145849d5c7d1071b1c3ba5e80c1fff5a8f
ayusshrathore <ayusshrathore@gmail.com>
2022-10-05 17:33:49 +0530

    Add code snippet for classes in javascript

---
+++ programs/javascript/classes.js
@@ -0,0 +1,31 @@
+class Animal {
+  constructor(age) {
+    this.age = age;
+  }
+
+  move() {
+    /* ... */
+  }
+}
+
+class Mammal extends Animal {
+  constructor(age, furColor) {
+    super(age);
+    this.furColor = furColor;
+  }
+
+  liveBirth() {
+    /* ... */
+  }
+}
+
+class Human extends Mammal {
+  constructor(age, furColor, languageSpoken) {
+    super(age, furColor);
+    this.languageSpoken = languageSpoken;
+  }
+
+  speak() {
+    /* ... */
+  }
+}
